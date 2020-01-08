617d55b03e6f1cc7a274a20fa6fe84b341c0ec0d
Garrett Hart <garrett7621@gmail.com>
2020-01-07 19:44:55 -0500

    init robotmap

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class Constants{
+    
+  //can ids
+    public static final int frontLeft = 0;
+  public static final int frontRight = 2;
+  public static final int backLeft = 1;
+  public static final int backRight = 3;
+
+  
+}
