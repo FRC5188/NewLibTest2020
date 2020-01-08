824e56fcc1d21e019231243cf5630109ad9a7c41
Garrett Hart <garrett7621@gmail.com>
2020-01-07 20:29:09 -0500

    init drive more

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,10 +3,11 @@ package frc.robot;
 public class Constants{
     
   //can ids
-    public static final int frontLeft = 0;
-  public static final int frontRight = 2;
-  public static final int backLeft = 1;
-  public static final int backRight = 3;
+    public static final int leftVictor = 0;
+  public static final int leftTalon = 2;
+  public static final int rightVictor = 1;
+  public static final int rightTalon = 3;
 
-  
+
+  public static final boolean isDriveCAN = true;
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,5 +1,54 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Constants;
+
+
 public class DriveTrain {
+
+    // private VictorSP leftDrive1;
+	// private VictorSP rightDrive1;
+	// private VictorSP leftDrive2;
+    // private VictorSP rightDrive2;
+
+    private VictorSPX leftVictor;
+	private VictorSPX rightVictor;
+	private TalonSRX leftTalon;
+    private TalonSRX rightTalon; 
+    
+    private DifferentialDrive robotDrive ;
+
+    public DriveTrain(){
+
+        if(Constants.isDriveCAN){
+            this.initCANMotors();
+            this.robotDrive = new DifferentialDrive(leftTalon, rightTalon);
+        }else{
+
+        }
+
+        
+
+    }
+
+    private void initCANMotors(){
+        leftVictor = new VictorSPX(Constants.leftVictor);
+            leftTalon = new TalonSRX(Constants.leftTalon);
+            rightVictor = new VictorSPX(Constants.rightVictor);
+            rightTalon = new TalonSRX(Constants.rightTalon);
+
+        leftVictor.follow(leftTalon);
+        rightVictor.follow(rightTalon);
+
+        
+    }
+
+    
+
 
 }
