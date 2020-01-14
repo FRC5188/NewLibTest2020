31b384892430573c183c51df9ebc4964ac074fc0
Killerskyman <kilerskyman@gmail.com>
2020-01-13 19:01:57 -0500

    add braking

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
@@ -40,6 +41,13 @@ public class DriveTrain {
         this.leftMotor2 = new TalonFX(1);
         this.rightMotor1 = new TalonFX(2);
         this.rightMotor2 = new TalonFX(3);
+
+        leftMotor1.setNeutralMode(NeutralMode.Brake);
+        leftMotor2.setNeutralMode(NeutralMode.Brake);
+        rightMotor1.setNeutralMode(NeutralMode.Brake);
+        rightMotor2.setNeutralMode(NeutralMode.Brake);
+
+
         this.leftMotor2.follow(leftMotor1);
         this.rightMotor2.follow(rightMotor1);
     }
